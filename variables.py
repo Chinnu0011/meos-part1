9a62531da513328751a99400a3001e30473d1ae3
chinnu <chinnumarnadu@gmail.com>
2026-06-06 20:29:04 +0530

    Day1: variables and data types

---
+++ variables.py
@@ -0,0 +1,17 @@
+#streak: Day 1
+#python variables and data types
+name = "suresh"
+age = 21
+city = "madurai"
+goal = "It professional"
+experience = 0
+is_learning = True
+threat_level = 9.5
+
+print(" ---PYTHON DAY 1 ---")
+print(f"my name is {name}.")
+print(f"i'm {age} years old")
+print(f"currently living in {city}")
+print(f"im having {experience} in this field")
+print(f"im currently learning python for cybersecurity {is_learning}")
+print(f"{threat_level}")
